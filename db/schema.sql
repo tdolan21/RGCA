4b434faf46d8f397a31dd3c317751b297d5ec93e
Kristen Kalmanson <kristenkalmanson@kristens-mac.local>
2018-11-17 13:17:30 -0500

    ok

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE burger_db;
+DROP DATABASE IF EXISTS rgca_db;
+CREATE DATABASE rgca_db;
 
-USE burger_db;
+USE rgca_db;
 
-CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY(id) 
+CREATE TABLE rgca_db (
+   id INT AUTO_INCREMENT NOT NULL,
+   user_name VARCHAR(255) NOT NULL,
+   devoured BOOLEAN DEFAULT false,
+   createdAt TIMESTAMP NOT NULL,
+   PRIMARY KEY(id)
 );
