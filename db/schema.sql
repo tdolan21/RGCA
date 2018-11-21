76dad7bfea2cf7cfd3ae9e1c65b3f42c507c9d36
Kristen Kalmanson <kris934@gmail.com>
2018-11-20 19:06:33 -0500

    merge

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,9 @@ CREATE DATABASE rgca_db;
 USE rgca_db;
 
 CREATE TABLE rgca_db (
-<<<<<<< HEAD
-   id INT AUTO_INCREMENT NOT NULL,
-   user_name VARCHAR(255) NOT NULL,
-   devoured BOOLEAN DEFAULT false,
-   createdAt TIMESTAMP NOT NULL,
-   PRIMARY KEY(id)
-=======
     id INT AUTO_INCREMENT NOT NULL,
     user_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY(id) 
->>>>>>> master
 );
